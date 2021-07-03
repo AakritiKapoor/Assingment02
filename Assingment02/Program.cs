bd35bfd53b79126811cc364b565827f9275da4a6
Aakriti Kapoor <akapoor1465@conestogac.on.ca>
2021-07-03 03:39:35 -0400

    Add project files.

---
+++ Assingment02/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assingment02
+{
+    class Program
+    {
+
+        public static int MenuOption()
+        {
+            string userSelelction = "";
+            bool selectionFlag = false;
+            while (selectionFlag == false)
+            {
+                Console.WriteLine("1 = Enter Triangle dimensions");
+                Console.WriteLine("2 = Exit\n");
+
+                Console.WriteLine("Please select an option, by entering a number:");
+                userSelelction = Console.ReadLine();
+                if (userSelelction != "1" &&
+                    userSelelction != "2")
+                {
+                    Console.WriteLine("This selection is invalid please enter the correct option from the menu:");
+
+                }
+                else { selectionFlag = true; }
+            }
+
+            return int.Parse(userSelelction);
+        }
+
+        public static double ValidateUserInput(string chosenLengthOfTriangle)
+        {
+            double aNumber = 1;
+            bool isValid = false;
+
+            while (isValid == false)
+            {
+                Console.WriteLine($"Please enter the {chosenLengthOfTriangle}:");
+                string userInput = Console.ReadLine();
+                Console.WriteLine();
+
+                bool result = double.TryParse(userInput, out aNumber);
+
+                if (result == false || aNumber < 1)
+                {
+                    Console.WriteLine("That's not a valid input please,  try again.\n");
+                }
+
+                else
+                {
+                    isValid = true;
+                    Console.WriteLine($"Current {chosenLengthOfTriangle} has been set to: {aNumber}.\n");
+                }
+            }
+
+            return aNumber;
+        }
+        static void Main(string[] args)
+        {
+            TriangleSolver triangleObj = new TriangleSolver();
+
+            int selection;
+            double firstSide, secondSide, thirdSide;
+            selection = MenuOption();
+
+            while (selection != 2)
+            {
+                firstSide = ValidateUserInput("First side");
+                secondSide = ValidateUserInput("Second side");
+                thirdSide = ValidateUserInput("Third side");
+                Console.WriteLine($"Entered dimentions for first side: {firstSide}, second side: {secondSide} and third side: {thirdSide}.\n");
+                TriangleSolver triangleObject2 = new TriangleSolver(firstSide, secondSide, thirdSide);
+                triangleObj = triangleObject2;
+                Console.WriteLine(triangleObj.Analyze());
+                selection = MenuOption();
+            }
+        }
+    }
+}
---
+++ Assingment02/TriangleSolver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assingment02
+{
+    public class TriangleSolver
+    {
+        private double firstSide;
+        private double secondSide;
+        private double thirdSide;
+
+        public TriangleSolver()
+        {
+            firstSide = 1;
+            secondSide = 1;
+            thirdSide = 1;
+        }
+
+        public TriangleSolver(double firstSide, double secondSide, double thirdSide)
+        {
+            this.firstSide = firstSide;
+            this.secondSide = secondSide;
+            this.thirdSide = thirdSide;
+        }
+        public string Analyze()
+        {
+            if(firstSide+secondSide<=thirdSide || firstSide+thirdSide<=secondSide || secondSide+thirdSide<=firstSide)
+            {
+                return "Does not from any triangle";
+            }
+            else {
+                if (firstSide == secondSide && secondSide == thirdSide) { return "Equilatral Triangle"; }
+                else if (firstSide == secondSide || firstSide == thirdSide || secondSide == thirdSide) { return "Isosceles Triangle "; }
+                else { return "Scalene Triangle"; }
+            }
+        }
+    }
+}
